2cfcd5766342c4354136546c19754f34a6821a65
Nick Prater <NikPratr@gmail.com>
2022-11-02 18:26:26 -0400

    empty commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 // const notes = require('./db/db.json');
 const uuid = require('./helpers/uuid.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
